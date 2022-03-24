8c29ae090e6991455e4a6196ed0ee2224c81a43e
Richard Lafon <richardlafon@hotmail.com>
2022-03-24 09:18:50 +0100

    fixed problem

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const info = require('./information');
 var cowsay = require("cowsay");
 
 console.log(cowsay.say({
-    text : `Hi, my name is ${Info.name} and I'm a wilder from ${Info.school}`,
+    text : `Hi, my name is ${info.name} and I'm a wilder from ${info.school}`,
     e : "oO",
     T : "U "
 }));
